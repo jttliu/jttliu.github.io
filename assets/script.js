b7be86e640d2ed54255e72a9936912628f75aaf2
Julie Liu <jttliu@berkeley.edu>
2016-11-29 21:49:11 -0800

    testttt

--- assets/script.js
+++ assets/script.js
@@ -3,17 +3,17 @@ $(document).ready(function() {
 	$("#navbar").hover(function() {
  		$("body").css("background", "#35465C");
 	});
-	$(".sidebar").click(function() {
-    if ($("#sidebar-button").hasClass("button-active")) {
-      $("body").removeClass("no-scroll")
-      $("#sidebar-button").removeClass("button-active")
-      $(".sidebar-container").removeClass("sidebar-active")
-      $(".page-wrapper").removeClass("wrapper-active")
-    }else{
-      $("body").addClass("no-scroll")
-      $("#sidebar-button").addClass("button-active")
-      $(".sidebar-container").addClass("sidebar-active")
-      $(".page-wrapper").addClass("wrapper-active")}
-  }
+	// $(".sidebar").click(function() {
+ //    if ($("#sidebar-button").hasClass("button-active")) {
+ //      $("body").removeClass("no-scroll")
+ //      $("#sidebar-button").removeClass("button-active")
+ //      $(".sidebar-container").removeClass("sidebar-active")
+ //      $(".page-wrapper").removeClass("wrapper-active")
+ //    }else{
+ //      $("body").addClass("no-scroll")
+ //      $("#sidebar-button").addClass("button-active")
+ //      $(".sidebar-container").addClass("sidebar-active")
+ //      $(".page-wrapper").addClass("wrapper-active")}
+ //  }
 
 });
